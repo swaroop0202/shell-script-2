b82b066152dcdee67a5c6cd79f6b66154f5051c4
swaroop0202 <swaroopk372@gmail.com>
2025-03-08 17:54:03 +0530

    M

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=bhuv
+PERSON2=druv
+
+echo "$PERSON1::Hi..How are u..?"
+echo "$PERSON2::Hi..im fine...wau..$PERSON1"
+echo "$PERSON1::im also good..what r u dng $PERSON2"
+echo "$PERSON2::im reading books..$PERSON1"
