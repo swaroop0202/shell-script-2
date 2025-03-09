431dd4b5d50145bd26951be058322e94dc93b22f
swaroop0202 <swaroopk372@gmail.com>
2025-03-09 12:48:24 +0530

    M

--- 05-functions.sh
+++ 05-functions.sh
@@ -8,3 +8,30 @@ echo "run the script with root user"
 else
 echo "you are root user"
 fi
+
+validate ()
+if [ $1 -ne 0]
+then
+    echo "$2...FAILURE"
+else
+    echo "$2...SUCCESS"
+
+
+dnf install mysql-server -y
+
+if [ $? -ne 0 ]
+then
+    echo "installation of mysql failure"
+else 
+    echo "installation success"
+fi
+
+systemctl enable mysqld
+
+if [ $? -ne 0 ] 
+then
+    echo "enable of mysql failure"
+else 
+    echo "enable success"
+fi
+
