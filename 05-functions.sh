6fc42aac39de89c657a9ed50b21bf37b8ce62294
swaroop0202 <swaroopk372@gmail.com>
2025-03-09 12:49:58 +0530

    M

--- 05-functions.sh
+++ 05-functions.sh
@@ -9,13 +9,13 @@ else
 echo "you are root user"
 fi
 
-validate ()
-if [ $1 -ne 0]
-then
-    echo "$2...FAILURE"
-else
-    echo "$2...SUCCESS"
-fi
+# validate ()
+# if [ $1 -ne 0]
+# then
+#     echo "$2...FAILURE"
+# else
+#     echo "$2...SUCCESS"
+# fi
 
 
 dnf install mysql-server -y
